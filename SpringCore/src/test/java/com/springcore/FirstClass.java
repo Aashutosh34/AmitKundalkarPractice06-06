9450bc71c17a4aecdc1d825914be32d18deeea87
amit <amit.kundalkar9@gmail.com>
2023-06-16 16:52:30 +0530

    6th change are done

--- SpringCore/src/test/java/com/springcore/FirstClass.java
+++ SpringCore/src/test/java/com/springcore/FirstClass.java
@@ -9,6 +9,9 @@ public class FirstClass {
 		System.out.println("Hello XYZ");
 		System.out.println("Hello PQR");
 		System.out.println("Hello LMN");
+		System.out.println("Hello LMN");
+		System.out.println("Hello Kalyani");
+
 
 
 
